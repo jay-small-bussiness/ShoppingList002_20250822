ede3da32eb33915df7baefd255949f0a83bd04d9
Junichiro Ishibashi <jay.small.business@outlook.com>
2025-08-25 08:24:29 +0900

    msg

--- Docs/minKai_history.mm
+++ Docs/minKai_history.mm
@@ -4,104 +4,51 @@
 <node CREATED="1755940684833" ID="ID_1483777715" MODIFIED="1755940684833" POSITION="right" TEXT="">
 <node CREATED="1755940685468" ID="ID_1265936410" MODIFIED="1755940689985" TEXT="minKai_history">
 <node CREATED="1755990581563" ID="ID_1306456666" MODIFIED="1755991663944" TEXT="2025.08.24">
-<node CREATED="1755990592131" ID="ID_1798458873" MODIFIED="1755990636820" TEXT="VoiceSearchViewModel&#x3078;&#x306e;EditCandidateItemPopup&#x547c;&#x3073;&#x51fa;&#x3057;&#x306e;&#x79fb;&#x690d;">
-<node CREATED="1755990643387" ID="ID_1730535219" MODIFIED="1755990666188" TEXT="CandidateCategoryViewModelCandidateCategoryViewModel &#x306e;&#x4e2d;&#x3067; EditCandidateItemPopup &#x3092;&#x958b;&#x3044;&#x3066;&#x308b;&#x7b87;&#x6240;&#x3001; &#x305d;&#x308c;&#x306f;&#x76f4;&#x63a5; Popup &#x3092; new &#x3057;&#x3066;&#x8868;&#x793a;&#x3057;&#x3066;&#x308b;&#x3093;&#x3084;&#x306a;&#x304f;&#x3066;&#x3001;&#x30a4;&#x30d9;&#x30f3;&#x30c8;&#x30c7;&#x30ea;&#x30b2;&#x30fc;&#x30c8;&#x7d4c;&#x7531;&#x3067; Page &#x306b;&#x51e6;&#x7406;&#x3092;&#x6295;&#x3052;&#x3066;&#x308b;&#x3093;&#x3084;&#x3002;"/>
-<node CREATED="1755990678417" ID="ID_411808926" MODIFIED="1755990679866" TEXT="&#x4ed5;&#x7d44;&#x307f;&#x306e;&#x6d41;&#x308c;">
-<node CREATED="1755990688048" ID="ID_1658896626" MODIFIED="1755990688048" TEXT="ViewModel &#x5074;&#x306b;&#x30c7;&#x30ea;&#x30b2;&#x30fc;&#x30c8;&#x3092;&#x6301;&#x3063;&#x3066;&#x308b;">
-<node CREATED="1755990699533" ID="ID_447459476" MODIFIED="1755990709223">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      public Func&lt;CandidateCategoryUiModel?, Task&gt; ShowPopupRequested { get; set; }
-    </p>
-  </body>
-</html>
-</richcontent>
+<node CREATED="1756077732969" ID="ID_327941999" MODIFIED="1756077732969" TEXT="">
+<node CREATED="1756077739101" ID="ID_1282414812" MODIFIED="1756077739101" TEXT="&#xd83d;&#xdccc; &#x30ab;&#x30c6;&#x30b4;&#x30ea;&#x8ffd;&#x52a0;&#x30fb;&#x7de8;&#x96c6; UI&#xff08;Popup&#x5468;&#x308a;&#xff09;&#x306e;&#x7d50;&#x8ad6;">
+<node CREATED="1756077739114" ID="ID_655573331" MODIFIED="1756077739114" TEXT="1. &#x73fe;&#x72b6;&#x306e;&#x554f;&#x984c;">
+<node CREATED="1756077739115" ID="ID_1290245981" MODIFIED="1756077739115" TEXT="EditCategoryPopupPage &#x3068;&#x3044;&#x3046;&#x540d;&#x524d;&#x3084;&#x3051;&#x3069;&#x5b9f;&#x614b;&#x306f; ContentPage"/>
+<node CREATED="1756077739116" ID="ID_1169871609" MODIFIED="1756077739116" TEXT="&#x547c;&#x3073;&#x51fa;&#x3057;&#x306f; PushModalAsync &#x3068; ShowPopupAsync &#x304c;&#x6df7;&#x5728;&#x3057;&#x3066;&#x3044;&#x3066;&#x4e2d;&#x9014;&#x534a;&#x7aef;"/>
+<node CREATED="1756077739116" ID="ID_613551825" MODIFIED="1756077739116" TEXT="&#x305d;&#x306e;&#x7d50;&#x679c;&#x3001;PopModalAsync failed because modal stack is currently empty &#x3084; Ambiguous routes matched &#x30a8;&#x30e9;&#x30fc;&#x304c;&#x767a;&#x751f;"/>
 </node>
 </node>
-<node CREATED="1755990719358" ID="ID_617979800" MODIFIED="1755990739319" TEXT="ViewModel &#x306e;&#x51e6;&#x7406;&#x304b;&#x3089;&#x30a4;&#x30d9;&#x30f3;&#x30c8;&#x767a;&#x706b;">
-<node CREATED="1755990728390" MODIFIED="1755990728390" TEXT="ShowPopupRequested?.Invoke(category);"/>
+<node CREATED="1756077739117" ID="ID_98588469" MODIFIED="1756077739117" TEXT="2. &#x3069;&#x3063;&#x3061;&#x306e;&#x65b9;&#x5f0f;&#x306b;&#x3059;&#x308b;&#x304b;">
+<node CREATED="1756077739119" ID="ID_1456858274" MODIFIED="1756077739119" TEXT="ModalPage &#x30b9;&#x30bf;&#x30a4;&#x30eb;&#xff08;PushModalAsync / PopModalAsync&#xff09;">
+<node CREATED="1756077739119" ID="ID_1489263139" MODIFIED="1756077739119" TEXT="&#x30d5;&#x30eb;&#x30b9;&#x30af;&#x30ea;&#x30fc;&#x30f3;&#x8868;&#x793a;&#x3067;&#x7de8;&#x96c6;&#x30d5;&#x30a9;&#x30fc;&#x30e0;&#x3092;&#x51fa;&#x3059;&#x306e;&#x306b;&#x5411;&#x3044;&#x3066;&#x308b;"/>
+<node CREATED="1756077739121" ID="ID_932984290" MODIFIED="1756077739121" TEXT="&#x623b;&#x308b;&#x30dc;&#x30bf;&#x30f3;&#x3084; Shell &#x306e;&#x30ca;&#x30d3;&#x30b2;&#x30fc;&#x30b7;&#x30e7;&#x30f3;&#x30b9;&#x30bf;&#x30c3;&#x30af;&#x3068;&#x81ea;&#x7136;&#x306b;&#x9023;&#x643a;"/>
+<node CREATED="1756077739121" ID="ID_1639652143" MODIFIED="1756077739121" TEXT="&#x4eca;&#x56de;&#x306e;&#x300c;&#x30ab;&#x30c6;&#x30b4;&#x30ea;&#x8ffd;&#x52a0;&#x30fb;&#x7de8;&#x96c6;&#x300d;&#x307f;&#x305f;&#x3044;&#x306a;&#x753b;&#x9762;&#x3067;&#x306f;&#x3053;&#x308c;&#x304c;&#x4e00;&#x822c;&#x7684;&#x3067;&#x5b89;&#x5b9a;"/>
 </node>
-<node CREATED="1755990736732" ID="ID_1591100524" MODIFIED="1755990737716" TEXT="Page &#x5074;&#x3067;&#x30c7;&#x30ea;&#x30b2;&#x30fc;&#x30c8;&#x306b;&#x51e6;&#x7406;&#x3092;&#x767b;&#x9332;&#x3057;&#x3066;&#x308b;">
-<node CREATED="1755990748041" ID="ID_1156672699" MODIFIED="1755990755559">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      viewModel.ShowPopupRequested = async category =&gt;
-    </p>
-    <p>
-      {
-    </p>
-    <p>
-      &#160;&#160;&#160;&#160;var popupVm = new EditCandidateItemPopupViewModel(...);
-    </p>
-    <p>
-      &#160;&#160;&#160;&#160;var popupPage = new EditCandidateItemPopup(popupVm);
-    </p>
-    <p>
-      &#160;&#160;&#160;&#160;await Navigation.PushModalAsync(popupPage);
-    </p>
-    <p>
-      };
-    </p>
-  </body>
-</html>
-</richcontent>
+<node CREATED="1756077739121" ID="ID_1546861788" MODIFIED="1756077739121" TEXT="Toolkit Popup &#x30b9;&#x30bf;&#x30a4;&#x30eb;&#xff08;ShowPopupAsync / Close&#xff09;">
+<node CREATED="1756077739121" ID="ID_410150257" MODIFIED="1756077739121" TEXT="&#x8efd;&#x91cf;&#x30aa;&#x30fc;&#x30d0;&#x30fc;&#x30ec;&#x30a4;&#x7528;&#xff08;&#x78ba;&#x8a8d;&#x30c0;&#x30a4;&#x30a2;&#x30ed;&#x30b0;&#x3084;&#x5c0f;&#x3055;&#x3044;&#x9078;&#x629e;UI&#x306b;&#x5411;&#x304f;&#xff09;"/>
+<node CREATED="1756077739121" ID="ID_1853845037" MODIFIED="1756077739121" TEXT="&#x4eca;&#x56de;&#x306e;&#x7528;&#x9014;&#x306b;&#x306f;&#x4e0d;&#x5411;&#x304d;"/>
 </node>
-<node CREATED="1755990775964" MODIFIED="1755990775964" TEXT="Page &#x304c;&#x300c;ShowPopupRequested &#x306b;&#x3053;&#x3046;&#x3044;&#x3046;&#x51e6;&#x7406;&#x3057;&#x3066;&#x306a;&#x300d;&#x3063;&#x3066;&#x767b;&#x9332;&#x3057;&#x3066;&#x308b;&#x3002;"/>
-<node CREATED="1755990775964" MODIFIED="1755990775964" TEXT="&#x3060;&#x304b;&#x3089; ViewModel &#x304b;&#x3089; Invoke &#x3055;&#x308c;&#x305f;&#x3089;&#x3001;Page &#x304c; Popup &#x3092;&#x958b;&#x304f;&#x3002;"/>
+<node CREATED="1756077739122" ID="ID_213328845" MODIFIED="1756077739122" TEXT="&#xd83d;&#xdc49; &#x7d50;&#x8ad6;&#xff1a;ModalPage &#x65b9;&#x5f0f;&#x306b;&#x7d71;&#x4e00;&#x3059;&#x308b;&#x306e;&#x304c;&#x6b63;&#x89e3;"/>
+</node>
+<node CREATED="1756077739124" ID="ID_896105295" MODIFIED="1756077739124" TEXT="3. ModalPage &#x65b9;&#x5f0f;&#x3067;&#x306e;&#x5b9f;&#x88c5;&#x30eb;&#x30fc;&#x30eb;">
+<node CREATED="1756077739124" ID="ID_255859393" MODIFIED="1756077739124" TEXT="&#x958b;&#x304f;&#x3068;&#x304d;">
+<node CREATED="1756077739125" ID="ID_1595111482" MODIFIED="1756077739125" TEXT="await Navigation.PushModalAsync(new EditCategoryPopupPage(vm));"/>
+</node>
+<node CREATED="1756077739125" ID="ID_370247320" MODIFIED="1756077739125" TEXT="&#x9589;&#x3058;&#x308b;&#x3068;&#x304d;">
+<node CREATED="1756077739126" ID="ID_843776271" MODIFIED="1756077739126" TEXT="await Shell.Current.Navigation.PopModalAsync();"/>
 </node>
 </node>
-<node CREATED="1755990792370" ID="ID_111417622" MODIFIED="1755990793085" TEXT="&#x3064;&#x307e;&#x308a;">
-<node CREATED="1755990805348" ID="ID_1366196879" MODIFIED="1755990810772">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p data-start="900" data-end="1016">
-      <code data-start="900" data-end="938">ShowPopupRequested.Invoke(category);</code>&#160;&#12399;<br data-start="940" data-end="943" /><strong data-start="943" data-end="959">&#12300;&#12452;&#12505;&#12531;&#12488;&#12434;&#25237;&#12370;&#12427;&#12384;&#12369;&#12301;</strong>&#12290;<br data-start="960" data-end="963" />&#23455;&#38555;&#12395; <code data-start="967" data-end="991">EditCandidateItemPopup</code>&#160;&#12434;&#38283;&#12356;&#12390;&#12427;&#12398;&#12399; <strong data-start="1000" data-end="1010">Page &#20596;</strong>&#12398;&#20966;&#29702;&#12290;
-    </p>
-  </body>
-</html>
-</richcontent>
+<node CREATED="1756077739126" ID="ID_1547480930" MODIFIED="1756077739126" TEXT="&#x4fdd;&#x5b58;&#x51e6;&#x7406;&#x306e;&#x6d41;&#x308c;">
+<node CREATED="1756077739126" ID="ID_64878449" MODIFIED="1756077739126" TEXT="Popup &#x5185;&#x3067;&#x300c;&#x4fdd;&#x5b58;&#x300d;&#x62bc;&#x3059;">
+<node CREATED="1756077739127" ID="ID_349770263" MODIFIED="1756077739127" TEXT="EditCategoryPopupViewModel.SaveAsync() &#x304c;&#x8d70;&#x308b;"/>
+<node CREATED="1756077739127" ID="ID_1808595556" MODIFIED="1756077739127" TEXT="onSaveCallback &#x3092;&#x901a;&#x3058;&#x3066; &#x89aa;&#x30da;&#x30fc;&#x30b8;&#x5074;&#x306e;&#x30e9;&#x30e0;&#x30c0;&#x3067; DB&#x4fdd;&#x5b58;&#x3084;&#x9077;&#x79fb;&#x3092;&#x5b9f;&#x884c;"/>
+<node CREATED="1756077739127" ID="ID_1028120680" MODIFIED="1756077739127" TEXT="SaveAsync &#x306e;&#x6700;&#x5f8c;&#x3067; PopModalAsync() &#x2192; &#x753b;&#x9762;&#x3092;&#x9589;&#x3058;&#x308b;"/>
 </node>
-<node CREATED="1755990805348" ID="ID_1417250895" MODIFIED="1755990850554">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p data-start="1023" data-end="1104">
-      &#12376;&#12421;&#12435;&#12385;&#12419;&#12435;&#12398; <code data-start="1034" data-end="1056">VoiceSearchViewModel</code>&#160; &#12363;&#12425; <code data-start="1060" data-end="1083">EditCategoryPopupPage</code>&#160;&#12434;&#38283;&#12365;&#12383;&#12356;&#12394;&#12425;&#12289;&#21516;&#12376;&#26041;&#24335;&#12395;&#12377;&#12427;&#12394;&#12425;&#65306;
-    </p>
-    <ul data-start="1106" data-end="1332">
-      <li data-start="1106" data-end="1218">
-        <p data-start="1108" data-end="1218">
-          <code data-start="1108" data-end="1130">VoiceSearchViewModel</code>&#160;&#12395; <code data-start="1133" data-end="1212">public Func&lt;CandidateCategoryUiModel?, Task&gt; ShowPopupRequested { get; set; }</code>&#160;&#12434;&#36275;&#12377;
-        </p>
-      </li>
-      <li data-start="1219" data-end="1267">
-        <p data-start="1221" data-end="1267">
-          Page &#20596;&#65288;VoiceSearchPage&#12392;&#12363;&#65311;&#65289;&#12391;&#12300;Popup&#12434;&#38283;&#12367;&#20966;&#29702;&#12301;&#12434;&#30331;&#37682;&#12377;&#12427;
-        </p>
-      </li>
-      <li data-start="1268" data-end="1332">
-        <p data-start="1270" data-end="1332">
-          ViewModel &#12363;&#12425;&#12399; <code data-start="1284" data-end="1325">ShowPopupRequested?.Invoke(newUiModel);</code>&#160; &#12391;&#25237;&#12370;&#12427;
-        </p>
-      </li>
-    </ul>
-  </body>
-</html>
-</richcontent>
 </node>
+<node CREATED="1756077739129" ID="ID_1151042412" MODIFIED="1756077739129" TEXT="4. &#x30bd;&#x30fc;&#x30b9;&#x6574;&#x7406;&#x306e;&#x65b9;&#x5411;&#x6027;">
+<node CREATED="1756077739129" ID="ID_1508929371" MODIFIED="1756077739129" TEXT="&#x300c;EditCategoryPopupPage&#x300d;&#x3068;&#x3044;&#x3046;&#x540d;&#x524d;&#x306e;&#x307e;&#x307e; ContentPage &#x3068;&#x3057;&#x3066; ModalPage &#x306b;&#x7d71;&#x4e00;"/>
+<node CREATED="1756077739129" ID="ID_1008069733" MODIFIED="1756077739129" TEXT="PushModalAsync / PopModalAsync &#x30da;&#x30a2;&#x306b;&#x63c3;&#x3048;&#x308b;"/>
+<node CREATED="1756077739129" ID="ID_1447384863" MODIFIED="1756077739129" TEXT="Toolkit Popup &#x306e; ShowPopupAsync &#x7cfb;&#x30b3;&#x30fc;&#x30c9;&#x306f;&#x524a;&#x9664;"/>
+<node CREATED="1756077739129" ID="ID_1723409527" MODIFIED="1756077739129" TEXT="&#x66d6;&#x6627;&#x30eb;&#x30fc;&#x30c8;&#x30a8;&#x30e9;&#x30fc;&#x306f; Popup&#x9589;&#x3058;&#x3066;&#x304b;&#x3089; GoToAsync &#x3067;&#x9077;&#x79fb;&#x3059;&#x308b; &#x3053;&#x3068;&#x3067;&#x89e3;&#x6d88;"/>
+</node>
+<node CREATED="1756077739131" ID="ID_1829783796" MODIFIED="1756077739131" TEXT="&#x2705; &#x7d50;&#x8ad6;">
+<node CREATED="1756077739131" ID="ID_1423530" MODIFIED="1756077739131" TEXT="&#xd83d;&#xdc49; &#x300c;&#x30ab;&#x30c6;&#x30b4;&#x30ea;&#x8ffd;&#x52a0;&#x30fb;&#x7de8;&#x96c6;&#x753b;&#x9762;&#x300d;&#x306f; ModalPage &#x65b9;&#x5f0f;&#xff08;PushModalAsync / PopModalAsync&#xff09;&#x306b;&#x4e00;&#x672c;&#x5316;&#x3059;&#x308b;&#x3002;"/>
+<node CREATED="1756077739131" ID="ID_605308029" MODIFIED="1756077739131" TEXT="&#x540d;&#x524d;&#x306f; EditCategoryPopupPage &#x306e;&#x307e;&#x307e;&#x3067;OK&#xff08;&#x4e2d;&#x8eab;&#x306f; ContentPage&#xff09;&#x3002;"/>
+<node CREATED="1756077739132" ID="ID_528673930" MODIFIED="1756077739132" TEXT="&#x3053;&#x308c;&#x3067;&#x5b9f;&#x88c5;&#x304c;&#x30b9;&#x30c3;&#x30ad;&#x30ea;&#x3057;&#x3066;&#x3001;&#x30eb;&#x30fc;&#x30c8;&#x3084;&#x30b9;&#x30bf;&#x30c3;&#x30af;&#x306e;&#x6df7;&#x4e71;&#x3082;&#x89e3;&#x6d88;&#x3059;&#x308b;&#x3002;"/>
 </node>
 </node>
 </node>
